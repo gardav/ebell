b02018a7211b1fe0467353682909530fdf8c5148
Gary Davies <gary.l.davies@gmail.com>
2024-01-07 17:15:29 +0000

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,26 @@ enum Stroke {
     Backstroke,
 }
 
-fn joystick() -> Result<(), String> {
+struct Buttons {
+    left: char,
+    right: char,
+}
+
+struct Bell {
+    key: char,
+    buttons: Buttons,
+}
+
+struct Strike {
+    stroke: Stroke,
+    time: u32,
+}
+
+fn key_click(enigo: &mut Enigo, c: char) {
+    enigo.key(Key::Unicode(c), Click).unwrap();
+}
+
+fn run(ebells: &[Bell]) -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let joystick_subsystem = sdl_context.joystick()?;
 
@@ -20,14 +39,17 @@ fn joystick() -> Result<(), String> {
     for id in 0..available {
         joysticks.push(joystick_subsystem.open(id).unwrap());
     }
-
-    let key = ['j', 'f'];
-    // let button = [['b', 'n'], ['g', 'S']]; // Ringing Room
-    let button = [['5', '6'], ['1', '2']]; // Mabel
     let debounce = 450;
-    let mut last_val = [0.0, 0.0];
-    let mut last_time = [0, 0];
-    let mut last_stroke = [Stroke::Backstroke, Stroke::Backstroke];
+    let left_bell_strike = Strike {
+        stroke: Stroke::Backstroke,
+        time: 0,
+    };
+    let right_bell_strike = Strike {
+        stroke: Stroke::Backstroke,
+        time: 0,
+    };
+    let mut last_strike = [right_bell_strike, left_bell_strike];
+    let mut last_position = [0.0, 0.0];
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
     let mut event_pump = sdl_context.event_pump()?;
     'running: loop {
@@ -42,36 +64,45 @@ fn joystick() -> Result<(), String> {
                     if axis_idx == 2 {
                         // Z axis
                         let bell = which as usize;
-                        let val = value as f32 / 16960.0;
-                        let lower_pos = -0.8;
-                        let upper_pos = 0.0;
-                        if val < lower_pos
-                            && last_val[bell] >= lower_pos
-                            && last_stroke[bell] == Stroke::Handstroke
-                            && timestamp - last_time[bell] > debounce
-                        {
-                            enigo.key(Key::Unicode(key[bell]), Click).unwrap();
-                            last_stroke[bell] = Stroke::Backstroke;
-                            last_time[bell] = timestamp;
-                        } else if val > upper_pos
-                            && last_val[bell] <= upper_pos
-                            && last_stroke[bell] == Stroke::Backstroke
-                            && timestamp - last_time[bell] > debounce
-                        {
-                            enigo.key(Key::Unicode(key[bell]), Click).unwrap();
-                            last_stroke[bell] = Stroke::Handstroke;
-                            last_time[bell] = timestamp;
+                        let position = value as f32 / 16960.0;
+                        let lower_limit = -0.8;
+                        let upper_limit = 0.0;
+                        let lower_limit_reached =
+                            position < lower_limit && last_position[bell] >= lower_limit;
+                        let upper_limit_reached =
+                            position > upper_limit && last_position[bell] <= upper_limit;
+                        let debounce_ok = timestamp - last_strike[bell].time > debounce;
+                        if debounce_ok {
+                            if lower_limit_reached && last_strike[bell].stroke == Stroke::Handstroke
+                            {
+                                key_click(&mut enigo, ebells[bell].key);
+                                last_strike[bell] = Strike {
+                                    stroke: Stroke::Backstroke,
+                                    time: timestamp,
+                                };
+                            } else if upper_limit_reached
+                                && last_strike[bell].stroke == Stroke::Backstroke
+                            {
+                                key_click(&mut enigo, ebells[bell].key);
+                                last_strike[bell] = Strike {
+                                    stroke: Stroke::Handstroke,
+                                    time: timestamp,
+                                };
+                            }
                         }
-                        last_val[bell] = val;
+                        last_position[bell] = position;
                     }
                 }
                 Event::JoyButtonDown {
                     which, button_idx, ..
                 } => {
                     let bell = which as usize;
-                    let side = button_idx as usize;
-                    let c = button[bell][side];
-                    enigo.key(Key::Unicode(c), Click).unwrap();
+                    let c = match button_idx {
+                        0 => ebells[bell].buttons.left,
+                        1 => ebells[bell].buttons.right,
+                        _ => panic!("button unknown"),
+                    };
+                    key_click(&mut enigo, c);
                 }
                 Event::Quit { .. } => break 'running,
                 _ => (),
@@ -83,5 +114,21 @@ fn joystick() -> Result<(), String> {
 }
 
 pub fn main() {
-    joystick().unwrap();
+    let left_bell = Bell {
+        key: 'f',
+        buttons: Buttons {
+            left: ';',  // Single
+            right: 'a', // Bob
+        },
+    };
+
+    let right_bell = Bell {
+        key: 'j',
+        buttons: Buttons {
+            left: 'g',  // Go
+            right: '.', // Stop
+        },
+    };
+    let ebells = [left_bell, right_bell];
+    run(&ebells).unwrap();
 }
